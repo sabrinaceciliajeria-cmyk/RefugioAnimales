41534e7a40532e8781afdde08b86a27b27b5c10e
Sabrina Jeria Figueroa <sabrinacecilia.jeria@gmail.com>
2026-04-09 18:15:14 -0400

    fix: eliminar implementación incorrecta de opción 3

--- src/RefugioAnimales.java
+++ src/RefugioAnimales.java
@@ -86,55 +86,10 @@ public class RefugioAnimales{
 
     public static void marcarComoAdoptado() {
         
-    //Filtramos la lista de animales para obtener solo los que están "Disponible"
-    List<String> disponibles = animales.stream()
-            .filter(a -> estadoAnimal.get(a).equals("Disponible")) // estado del animal
-            .collect(Collectors.toList()); // convertimos el Stream en una lista
+    
+    public static void mostrarAnimalesAdoptados() {
 
-    // Si no hay animales disponibles, mostramos mensaje y salimos del método
-    if (disponibles.isEmpty()) {
-        System.out.println("No hay disponibles.");
-        return;
     }
-
-    // Mostramos los animales disponibles numerados para que el usuario elija
-    System.out.println("Animales disponibles:");
-
-    for (int i = 0; i < disponibles.size(); i++) {
-        System.out.println((i + 1) + ". " + disponibles.get(i));
-    }
-
-    // Pedimos al usuario que seleccione un animal por número
-    System.out.print("Seleccione: ");
-
-    int op;
-
-    try {
-        // Convertimos la entrada del usuario a número entero
-        op = Integer.parseInt(sc.nextLine());
-
-        // Validamos que la opción esté dentro del rango de la lista
-        if (op < 1 || op > disponibles.size()) {
-            System.out.println("Opción inválida.");
-            return;
-        }
-
-    } catch (Exception e) {
-        // Si el usuario no escribe un número válido
-        System.out.println("Debe ingresar número.");
-        return;
-    }
-
-    // Obtenemos el animal seleccionado según la posición elegida
-    String seleccionado = disponibles.get(op - 1);
-
-    // Cambiamos el estado del animal a "Adoptado"
-    estadoAnimal.put(seleccionado, estados[1]);
-
-    // Confirmamos la adopción
-    System.out.println("✔ " + seleccionado + " adoptado.");
-}
-
     public static void mostrarAnimalesDisponibles() {
 
     }
