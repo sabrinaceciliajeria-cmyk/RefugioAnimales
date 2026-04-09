b94d98a20b6c347fad02405b98abf19479f81fb5
Sabrina Jeria <sabrinacecilia.jeria@gmail.com>
2026-04-09 15:49:11 -0400

    Subir código completo de RefugioAnimales.java con estructura de datos y comentarios

---
+++ src/RefugioAnimales.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class RefugioAnimales {
+
+    public static void main(String[] args) {
+
+        int opcion;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("===BIENVENIDO AL REFUGIO===");
+            System.out.println("1. Registrar Animal");
+            System.out.println("2. Registrar Especie");
+            System.out.println("3. Marcar Animal como Adoptado");
+            System.out.println("4. Mostrar Animales Disponibles");
+            System.out.println("5. Mostrar Animales Adoptados");
+            System.out.println("6. Mostrar Reporte General");
+            System.out.println("0. Salir");
+
+            switch (opcion) {
+                case 1 -> {
+                }
+
+                case 2 -> {
+                }
+
+                case 3 -> {
+
+                }
+
+                case 4 ->{
+
+                }
+
+                case 5 ->{
+
+                }
+
+                case 6 ->{
+
+                }
+
+                case 7 -> {
+
+                }
+                default ->
+                        System.out.println("Opcion no valida");
+
+            }
+        }while(true);
+
+    }
+
+    public static void registrarAnimal() {
+
+    }
+
+    public static void registrarEspecie() {
+
+    }
+
+    public static void marcarComoAdoptado() {
+    }
+
+    public static void mostrarAnimalesDisponibles() {
+
+    }
+
+    public static void mostrarAnimalesAdoptados() {
+
+    }
+
+    public static void reporteGeneral() {
+    }
+}
